e89d8997d1f79092398cc6431a5295ee38d7e7ea
edu-salcedo <edusalcedopaucar@gmail.com>
2023-11-18 17:36:57 -0300

    navbar lateral

---
+++ JS/index.js
@@ -0,0 +1,40 @@
+
+// constantes para los items del menu de navegacion
+ const navbarItems=document.querySelector(".sidebar").querySelectorAll("li");
+ const navbar=document.querySelector(".sidebar");
+
+ navbarItems.forEach(element => {
+    element.addEventListener("click",function(){
+        navbarItems.forEach(nav=>nav.classList.remove("active")); 
+           this.classList.add("active");
+    })
+});
+// constantes para el menu de navegacion
+const close=document.querySelector(".close-btn");
+const open=document.querySelector(".open-btn");
+
+open.addEventListener("click",() => {
+     navbar.classList.add("active");
+     console.log(navbar.classList);
+})
+
+close.addEventListener("click",() => {
+    navbar.classList.remove("active");
+})
+
+
+
+
+
+
+
+// const toggle= document.querySelector(".nav-toggle");
+// const nav_menu= document.querySelector(".nav-menu");
+// const nav_link =document.querySelector(".nav-link");
+// toggle.addEventListener("click",()=>{
+//     nav_menu.classList.toggle("menu-active");
+// })
+
+// nav_link.addEventListener("click",()=>{
+//     nav_menu.classList.remove("menu-active")
+// })
