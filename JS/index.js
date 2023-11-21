d673689c78d71d9aacf2b6ce9129516cfefa1846
edu-salcedo <edusalcedopaucar@gmail.com>
2023-11-21 19:31:17 -0300

    seccion proyectos

--- JS/index.js
+++ JS/index.js
@@ -45,3 +45,25 @@ open.addEventListener("click",() => {
 // nav_link.addEventListener("click",()=>{
 //     nav_menu.classList.remove("menu-active")
 // })
+
+
+
+var swiper = new Swiper(".swiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 2,
+      slideShadows: true
+    },
+    spaceBetween: 60,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    }
+  });
